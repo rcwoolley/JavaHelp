42fe67d638fa65b5065fcd5ffb9a6d42b8f981bb
Rob Woolley <rcwoolley@gmail.com>
2015-10-17 14:39:58 -0400

    Extend example to invoke constructor to create a new instance

--- HelpingEthan/src/HelpingEthan.java
+++ HelpingEthan/src/HelpingEthan.java
@@ -1,23 +1,29 @@
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Constructor;
 
 public class HelpingEthan {
     public static void main(String[] args) {
         System.out.println("Hello, World");
-        
-        Scalar myScalar = new Scalar("myScalar");
-        System.out.println("Scalar object's AlgName is: "+myScalar.getAlgName());
-        
+               
         Class[] theArray = new Class[10];
         theArray[1] = Scalar.class;
 
         try {
+            Class cls[] = new Class[] { String.class };
+            Constructor myConstructor = theArray[1].getConstructor(cls);
+            
+            Scalar myScalar = (Scalar)myConstructor.newInstance(new Object[] { "myScalar" });
+
+            System.out.println("Scalar object's AlgName is: "+myScalar.getAlgName());
+
         	Method myMethod = theArray[1].getMethod("getAlgName");
             Object myAlgName = myMethod.invoke(myScalar, null);
             System.out.println("Scalar object's AlgName is: "+myAlgName);
-
         } catch (NoSuchMethodException e) {
         	System.out.println("No such method exists");
+        } catch (InstantiationException e) {
+        	System.out.println("Can not instantiate");
         } catch (InvocationTargetException e) {
         	System.out.println("Invocation target exception");
         } catch (IllegalAccessException e) {
